37b3f2dff44345108dfc66ea54b42663b75eb3c0
Jasraj Bhatia <j.b60980@gmail.com>
2020-11-14 17:39:17 +0400

    All subjects and classes added

--- products.js
+++ products.js
@@ -30,5 +30,125 @@ let products = [
         "rating": 4,
         "quality": 5
     },
+    {
+        "id": 4,
+        "subject": "Biology",
+        "location": "Oxford",
+        "price": "25",
+        "image": "images/biology_subject.jpg",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+    {
+        "id": 5,
+        "subject": "Chemistry",
+        "location": "Liverpool",
+        "price": "30",
+        "image": "images/chemistry_subject.jpg",
+        "spaces": 5,
+        "rating": 2,
+        "quality": 5
+    },
+    {
+        "id": 6,
+        "subject": "Computer Science",
+        "location": "London",
+        "price": "70",
+        "image": "images/computerscience_subject.jpg",
+        "spaces": 5,
+        "rating": 5,
+        "quality": 5
+    },
+    {
+        "id": 7,
+        "subject": "Business",
+        "location": "Liverpool",
+        "price": "40",
+        "image": "images/business_subject.jpg",
+        "spaces": 5,
+        "rating": 5,
+        "quality": 5
+    },
+    {
+        "id": 8,
+        "subject": "Psychology",
+        "location": "Liverpool",
+        "price": "35",
+        "image": "images/psychology_subject.png",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+    {
+        "id": 9,
+        "subject": "Law",
+        "location": "Oxford",
+        "price": "20",
+        "image": "images/law_subject.jpg",
+        "spaces": 5,
+        "rating": 3,
+        "quality": 5
+    },
+    {
+        "id": 10,
+        "subject": "Music",
+        "location": "London",
+        "price": "60",
+        "image": "images/music_subject.jpg",
+        "spaces": 5,
+        "rating": 2,
+        "quality": 5
+    },
+    {
+        "id": 11,
+        "subject": "Dance",
+        "location": "Liverpool",
+        "price": "30",
+        "image": "images/dance_subject.jpg",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+    {
+        "id": 12,
+        "subject": "Soccer",
+        "location": "London",
+        "price": "70",
+        "image": "images/soccer_subject.jpg",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+    {
+        "id": 13,
+        "subject": "Badminton",
+        "location": "Oxford",
+        "price": "50",
+        "image": "images/badminton_subject.jpg",
+        "spaces": 5,
+        "rating": 3,
+        "quality": 5
+    },
+    {
+        "id": 14,
+        "subject": "Archery",
+        "location": "London",
+        "price": "90",
+        "image": "images/archery_subject.jpg",
+        "spaces": 5,
+        "rating": 5,
+        "quality": 5
+    },
+    {
+        "id": 15,
+        "subject": "Shooting",
+        "location": "Oxford",
+        "price": "90",
+        "image": "images/shooting_subject.jpg",
+        "spaces": 5,
+        "rating": 5,
+        "quality": 5
+    },
 
 ]
