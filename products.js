ce5d6b2b2bbdc35b03c90960804af5e672199f55
Jasraj Bhatia <j.b60980@gmail.com>
2020-11-14 12:15:16 +0400

    Added few subjecrts

---
+++ products.js
@@ -0,0 +1,34 @@
+let products = [
+    // ADD IMAGE in the products 
+    {
+        "id": 1,
+        "subject": "Math",
+        "location": "Liverpool",
+        "price": "25",
+        "image": "images/math-subject.jpg",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+    {
+        "id": 2,
+        "subject": "English",
+        "location": "London",
+        "price": "20",
+        "image":"images/english_subject.jpg",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+    {
+        "id": 3,
+        "subject": "Physics",
+        "location": "Oxford",
+        "price": "30",
+        "image": "images/physics_subject.jpg",
+        "spaces": 5,
+        "rating": 4,
+        "quality": 5
+    },
+
+]
